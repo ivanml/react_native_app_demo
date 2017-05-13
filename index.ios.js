e7bb584c9acf71284b185ac503b88e38ba1f9bed
Lei Mao <ivan@new-host-3.home>
2017-05-12 23:47:56 -0400

    initial phase with a list view of movies

---
+++ index.ios.js
@@ -0,0 +1,130 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {
+  Component
+} from 'react';
+import {
+  AppRegistry,
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+export default class AwesomeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+    this.renderMovie = this.renderMovie.bind(this); // this is needed if renderMovie() is going to call some other this.functions
+  }
+
+  // only called once by React
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: this.replaceHttp(movie.posters.thumbnail)}}
+          //source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  // utility function to change http to https in uri
+  replaceHttp(input) {
+    return 'https://' + input.split('//')[1];
+  }
+}
+
+// styles
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
